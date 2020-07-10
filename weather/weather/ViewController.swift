d7999a3410b01c6cb90b59158cec5bd67483b113
luizfelipeairesoares <lfelipeas@gmail.com>
2020-07-10 20:20:36 -0300

    :tada: Creates project

---
+++ weather/weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weather
+//
+//  Created by Luiz Felipe Aires Soares on 10/07/20.
+//  Copyright © 2020 luizfelipeairesoares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
