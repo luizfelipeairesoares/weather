7c367fe3202db79a748925f197dee4f98dcba5e3
luizfelipeairesoares <lfelipeas@gmail.com>
2020-07-19 16:44:32 -0300

    :sparkles: Creates CoordinatorProtocol

---
+++ weather/weather/Utils/Protocols/CoordinatorProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  CoordinatorProtocol.swift
+//  weather
+//
+//  Created by Luiz Felipe Aires Soares on 19/07/20.
+//  Copyright © 2020 luizfelipeairesoares. All rights reserved.
+//
+
+import UIKit
+
+protocol CoordinatorProtocol {
+    
+    func start() -> UIViewController
+    
+}
