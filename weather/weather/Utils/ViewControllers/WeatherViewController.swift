9b227f4c20baaae9f2a25449e34a9a4bfd624030
luizfelipeairesoares <lfelipeas@gmail.com>
2020-07-19 16:40:04 -0300

    :sparkles: Creates WeatherViewController

---
+++ weather/weather/Utils/ViewControllers/WeatherViewController.swift
@@ -0,0 +1,46 @@
+//
+//  WeatherViewController.swift
+//  weather
+//
+//  Created by Luiz Felipe Aires Soares on 19/07/20.
+//  Copyright © 2020 luizfelipeairesoares. All rights reserved.
+//
+
+import UIKit
+import NVActivityIndicatorView
+
+class WeatherViewController: UIViewController, ViewControllerProtocol {
+    
+    // MARK: - Properties
+    
+    private(set) lazy var loading: NVActivityIndicatorView = {
+        let activity = NVActivityIndicatorView(frame: .zero, type: .circleStrokeSpin, color: .white, padding: nil)
+        activity.translatesAutoresizingMaskIntoConstraints = false
+        return activity
+    }()
+    
+    // MARK: - View Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(loading)
+        NSLayoutConstraint.activate([
+            loading.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loading.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loading.widthAnchor.constraint(equalToConstant: 64.0),
+            loading.heightAnchor.constraint(equalToConstant: 64.0)
+        ])
+    }
+    
+    // MARK: - ViewControllerProtocol Functions
+    
+    func showLoading() {
+        loading.startAnimating()
+    }
+    
+    func hideLoading() {
+        loading.stopAnimating()
+    }
+
+}
